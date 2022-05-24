7d56518a96dac8a1a9677f4d59ed0c21aa640246
ireaneus <bwdave@protonmail.com>
2022-05-24 17:32:10 -0600

    Appbar setup with display container for menu items using display-container

--- src/components/Appbar.jsx
+++ src/components/Appbar.jsx
@@ -13,10 +13,11 @@ const DATEOPTIONS= {
 const DisplayDate = new Date().toLocaleDateString('en-US', DATEOPTIONS);
 
   return (
-          <div className='w3-container w3-theme-d1 w3-card-4'>
-            <div className='w3-bar'>
-              <button className='w3-button w3-right w3-hover-blue-grey' sx={{display:{xs: 'none', sm: 'none', md: 'block'} }} >{ DisplayDate }</button>
-              <h6 className='w3-wide w3-center white' >Grace Fellowship of Mountain Home Idaho</h6>
+          <div className='w3-container w3-top w3-theme-d1 w3-card-4'>
+            <div className='w3-display-container'>
+            <button className="w3-button w3-display-left w3-hover-blue-grey" onclick="w3_open()"><i className="fas fa-church"></i></button>
+              <button className='w3-button w3-display-right w3-hover-blue-grey' >{ DisplayDate }</button>
+              <h6 className='w3-wide w3-display-center white' >Grace Fellowship of Mountain Home Idaho</h6>
             </div>
           </div>
   );
